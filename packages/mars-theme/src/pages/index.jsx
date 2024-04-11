29de951be14780c9593cfcc7cb89bad56d613328
TommyKooij <74959713+TommyKooij@users.noreply.github.com>
2024-04-11 14:06:33 +0200

    11/04/2024

--- packages/mars-theme/src/pages/index.jsx
+++ packages/mars-theme/src/pages/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { styled } from "frontity";
+import cover from "../img/IMG_4826.jpg";
 
 export function HomePage(props) {
   return (
     <Container>
       <Title>Welcome to Frontity!</Title>
+      <StyledImage alt={"cover"} src={cover} width={"100%"} height={"100%"} />
     </Container>
   );
 }
@@ -20,4 +22,11 @@ const Title = styled.h1`
   margin-top: 24px;
   margin-bottom: 8px;
   color: rgba(12, 17, 43);
+`;
+
+const StyledImage = styled.img`
+  display: block;
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
 `;
